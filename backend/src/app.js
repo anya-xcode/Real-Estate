df58cff2957e1b34a7f4220e339f1426d7d08dc0
codervipul775 <ydvvipul2005@gmail.com>
2025-11-29 22:16:31 +0530

    admin issue fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,7 +19,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-admin-password']
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
